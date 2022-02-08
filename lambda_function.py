c1a88f84b7288215fe6b7435070bff6e35fcb7ce
Prabhat Dwivedie <64556857+prabhatpp@users.noreply.github.com>
2022-02-08 13:57:31 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
-Import pandas as pd 
+import json
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.Dataframe(data=d)
-    print(df)
-    print('Done x1.1')
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!....... (Modified v0.2')
+    }
